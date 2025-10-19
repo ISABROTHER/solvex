d70b731fa82ac1bb26f5d887875580df8de6dbe5
ISABROTHER <ishimpatrickjohnston@gmail.com>
2025-10-19 13:44:15 +0200

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
-// src/main.tsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastProvider } from './contexts/ToastContext.tsx';
+import { CartProvider } from './contexts/CartContext.tsx';
+// Ensure AuthProvider is imported correctly
+import { AuthProvider } from './features/auth'; // Or './features/auth/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <ToastProvider>
+        <CartProvider>
+          {/* Ensure AuthProvider wraps App */}
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </CartProvider>
+      </ToastProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
