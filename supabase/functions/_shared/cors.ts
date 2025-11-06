6f171c85c8748b63c96cdd6133edf69776307cd3
ISABROTHER <ishimpatrickjohnston@gmail.com>
2025-11-06 17:53:43 +0100

    Updated cors.ts

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,3 +1,4 @@
+// This file allows your website to securely communicate with Supabase Functions.
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
