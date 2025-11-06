2faf0ffb875ba7b68b7d9e408b154edc9dc11489
ISABROTHER <ishimpatrickjohnston@gmail.com>
2025-11-06 11:28:13 +0100

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -200,7 +200,7 @@ const HomePage = () => {
           className="absolute top-0 left-0 w-full h-full object-cover z-0"
         >
           <source
-            src="https://videos.pexels.com/video-files/7821854/7821854-hd_1920_1080_30fps.mp4"
+            src="https://videos.pexels.com/video-files/3254008/3254008-hd_1920_1080_25fps.mp4"
             type="video/mp4"
           />
         </video>
